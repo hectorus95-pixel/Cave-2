2b3e7039633c38d8d12dad83205547b10aa38bc6
hectorus95-pixel <hectorus95@gmail.com>
2026-09-10 20:24:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
-const C='ma-cave-configurable-v6-7';
-const A=['./','./index.html','./app.js?v=6.7','./manifest.webmanifest?v=6.7'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)));});
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ma-cave-configurable-')&&k!==C).map(k=>caches.delete(k))))])));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request))));
+const C='ma-cave-configurable-v6-8';
+const A=['./','./index.html','./app-v6-8.js?v=60800','./manifest.webmanifest?v=6.8'];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)));
+});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k.startsWith('ma-cave-configurable-')&&k!==C).map(k=>caches.delete(k))
+  ))
+])));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(C).then(c=>c.put(e.request,copy));
+    return r;
+  }).catch(()=>caches.match(e.request))
+));
